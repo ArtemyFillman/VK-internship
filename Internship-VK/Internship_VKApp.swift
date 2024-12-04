9d8b122c9ccc2664cf887fe566bb45f2a1649aa4
Artemy Fillman <mainfillman@gmail.com>
2024-12-05 00:26:45 +0300

    Initial Commit

---
+++ Internship-VK/Internship_VKApp.swift
@@ -0,0 +1,17 @@
+//
+//  Internship_VKApp.swift
+//  Internship-VK
+//
+//  Created by Artemy Fillman on 05/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct Internship_VKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Internship-VKTests/Internship_VKTests.swift
@@ -0,0 +1,17 @@
+//
+//  Internship_VKTests.swift
+//  Internship-VKTests
+//
+//  Created by Artemy Fillman on 05/12/2024.
+//
+
+import Testing
+@testable import Internship_VK
+
+struct Internship_VKTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
